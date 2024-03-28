a068188f96099c006c168d642b0939f428f1999c
chems eddine bourabia <bourabiachemseddine@gmail.COM>
2024-03-28 01:35:57 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,17 +3,35 @@ const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
 const countlabel = document.getElementById("count");
 let count=0;
-reset.onclick= function () {
+reset.onmousedown= function () {
     count=0;
     countlabel.textContent=count;
+    reset.style.width="9.3vw";
+    reset.style.height="4.3vw";
+    
 }
-increase.onclick= function () {
-    count++;
-    countlabel.textContent=count;
-
+reset.onmouseup= function () {
+    reset.style.width="9vw";
+    reset.style.height="4vw";
 }
-decrease.onclick= function () {
+increase.onmousedown= function () {
+    increase.style.width="9.3vw";
+    increase.style.height="4.3vw";
+        count++;
+        countlabel.textContent=count; 
+}
+increase.onmouseup= function () {
+    increase.style.width="9vw";
+    increase.style.height="4vw";
+}
+decrease.onmousedown= function () {
     count--;
     countlabel.textContent=count;
+    decrease.style.width="9.3vw";
+    decrease.style.height="4.3vw";
 
+}
+decrease.onmouseup= function () {
+    decrease.style.width="9vw";
+    decrease.style.height="4vw";
 }
